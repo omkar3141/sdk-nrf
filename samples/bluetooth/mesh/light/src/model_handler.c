cbb6ca1388882f741da143b2a7d2da045126bac6
Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>
2023-10-06 15:49:06 +0200

    mesh: Test suspend resume
    Tests suspend resume feature using DK buttons.
    
    Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

--- samples/bluetooth/mesh/light/src/model_handler.c
+++ samples/bluetooth/mesh/light/src/model_handler.c
@@ -42,6 +42,74 @@ static struct led_ctx led_ctx[] = {
 	{ .srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers) },
 #endif
 };
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	err = bt_mesh_resume();
+	if (err) {
+		printk("Mesh resume failed (err %d)\n", err);
+		return;
+	}
+	printk("Mesh resumed\n");
+}
+
+static void button_handler_cb(uint32_t pressed, uint32_t changed)
+{
+	if (!bt_mesh_is_provisioned()) {
+		return;
+	}
+
+	static bool is_suspended = false;
+	int err;
+	/* If button 0 is pressed, toggle bluetooth and mesh stack operation (suspend/resume) */
+	if (pressed & changed & BIT(0)) {
+		printk("Button 0 pressed\n");
+		if (!is_suspended) {
+			printk("disabling Bluetooth\n");
+			err = bt_mesh_suspend();
+			if (err) {
+				printk("Mesh suspend failed (err %d)\n", err);
+			}
+			printk("disabling mesh\n");
+			err = bt_disable();
+			if (err) {
+				printk("Bluetooth disable failed (err %d)\n", err);
+			}
+			printk("both disabled\n");			
+		} else {
+			err = bt_enable(bt_ready);
+			if (err) {
+				printk("Bluetooth enable failed (err %d)\n", err);
+			}	
+			printk("Waiting for bluetooth to be ready\n");				
+		}
+		is_suspended = !is_suspended;
+		return;
+	}
+
+	/* If button 1 is pressed, toggle only mesh stack operation (suspend/resume) */
+	if (pressed & changed & BIT(1)) {
+		printk("Button 1 pressed\n");
+		if (!is_suspended) {
+			printk("disabling mesh\n");
+			err = bt_mesh_suspend();
+			if (err) {
+				printk("Mesh suspend failed (err %d)\n", err);
+			}
+		} else {
+			printk("mesh resuming\n");
+			err = bt_mesh_resume();
+			if (err) {
+				printk("Mesh resume failed (err %d)\n", err);
+			}	
+		}
+		is_suspended = !is_suspended;
+	}
+}
 
 static void led_transition_start(struct led_ctx *led)
 {
@@ -217,6 +285,12 @@ const struct bt_mesh_comp *model_handler_init(void)
 	for (int i = 0; i < ARRAY_SIZE(led_ctx); ++i) {
 		k_work_init_delayable(&led_ctx[i].work, led_work);
 	}
+
+	static struct button_handler button_handler = {
+		.cb = button_handler_cb,
+	};
+
+	dk_button_handler_add(&button_handler);
 
 	return &comp;
 }
